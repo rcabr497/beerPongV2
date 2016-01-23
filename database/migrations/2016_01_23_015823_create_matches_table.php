7caef131a2f9ad93a0495a5892df0595337bbe42
rcabr497 <info@rickycabrera.com>
2016-01-22 21:10:00 -0500

    new table migrations

---
+++ database/migrations/2016_01_23_015823_create_matches_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMatchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('matches', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('winning_team', 30)->nullable();
+            $table->string('losing_team', 30)->nullable();
+            $table->text('game_date');
+            $table->integer('cups_left');
+            $table->integer('overtime');
+            $table->text('comments');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
---
+++ database/migrations/2016_01_23_015852_create_teams_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('matches', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 60);
+            $table->string('player1', 60);
+            $table->string('player2', 60)->nullable();
+            $table->dateTime('created_at');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
